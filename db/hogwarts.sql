921f2287eef0b8616cd8ea57c886448cf24d72a4
Shaun Jerdan <s.jerdan93@gmail.com>
2020-03-18 11:50:59 +0000

    part1 commit

---
+++ db/hogwarts.sql
@@ -0,0 +1,9 @@
+DROP TABLE students;
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY,
+  first_name varchar(255),
+  last_name varchar(255),
+  house VARCHAR(255),
+  age INT
+);
---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+require_relative('../models/hogwarts')
+
+student1 = Student.new({
+  "first_name" => "Harry",
+  'last_name' => "Potter",
+  "house" => "Gryffindor",
+  "age" => 16
+  })
+
+student2 = Student.new({
+  "first_name" => "Hermione",
+  "last_name" => "Granger",
+  "house" => "Gryffindor",
+  "age" => 16
+  })
+
+student3 =  Student.new({
+  "first_name" => "Draco",
+  "last_name" => "Malfoy",
+  "house" => "Slytherin",
+  "age" => 16
+
+  })
+
+student1.save
+student2.save
+student3.save
